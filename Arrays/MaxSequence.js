4c788dd660c1895945250043f1822cd7b7319963
ekolev11 <evgenikolev11@gmail.com>
2023-10-04 17:56:13 +0300

    Update MaxSequence.js

--- Arrays/MaxSequence.js
+++ Arrays/MaxSequence.js
@@ -1,15 +1,20 @@
 function solve(arr1){
-    let result = [];
     let arr = arr1;
+    let longestSequence = [];
+    let currentSequence = [];
     for (i = 0; i < arr.length; i++){
-
         if (arr[i] === arr[i - 1]){
-            result.push(arr[i]);
-            result.push(arr[i - 1])
+           currentSequence.push(arr[i]);
+           currentSequence.push(arr[i - 1]);
+        }else{
+            longestSequence = currentSequence;
+            currentSequence = [];
         }
+
+        
         
         
     }
-    console.log(result);
+    console.log(longestSequence);
 
 }solve([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
