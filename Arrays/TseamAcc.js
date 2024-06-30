ca555b50b077b53219c0d6af4c5d75ae5b911d6c
ekolev11 <evgenikolev11@gmail.com>
2024-06-30 15:57:05 +0200

    Create TseamAcc.js

---
+++ Arrays/TseamAcc.js
@@ -0,0 +1,14 @@
+function solve(arr){
+    let index = 0;
+    let command = "";
+    let myGames = command[0];
+    // while(command[index] !== "Play!"){
+
+
+    // }
+    let result = arr[1].split(" ");
+
+    console.log(result[1]);
+
+
+}solve(['CS WoW Diablo', 'Install LoL', 'Uninstall WoW', 'Update Diablo', 'Expansion CS-Go', 'Play!'])
